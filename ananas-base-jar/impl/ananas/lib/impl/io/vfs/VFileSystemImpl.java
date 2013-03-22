a3888cb76b8a82b0af3967ae330f89ad18750cd9
xukun <xukun0217@gmail.com>
2013-03-23 05:21:41 +0800

    add: vfs impl

---
+++ ananas-base-jar/impl/ananas/lib/impl/io/vfs/VFileSystemImpl.java
@@ -0,0 +1,53 @@
+package ananas.lib.impl.io.vfs;
+
+import java.net.URI;
+
+import ananas.lib.io.vfs.VFile;
+import ananas.lib.io.vfs.VFileSystem;
+import ananas.lib.io.vfs.VFileSystemConfiguration;
+import ananas.lib.io.vfs.VFileSystemFactory;
+
+public class VFileSystemImpl implements VFileSystem {
+
+	public VFileSystemImpl(VFileSystemFactory factory,
+			VFileSystemConfiguration config) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public VFileSystemFactory getFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VFileSystemConfiguration getConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VFile newFile(VFile dir, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VFile newFile(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VFile newFile(String dir, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VFile newFile(URI uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
